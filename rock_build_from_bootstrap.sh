7b4eaf4df646d55be75062499ee79d7fcd43b4bd
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-08-26 08:51:08 +0200

    from_bootstrap: fix test for non-default configurations

--- rock_build_from_bootstrap.sh
+++ rock_build_from_bootstrap.sh
@@ -6,7 +6,7 @@ export PATH=/home/build/rock_admin_scripts/bin:$PATH
 CONFIG_DIR=/home/build/slave_conf
 
 job_basename=`dirname $JOB_NAME`
-if test -f /home/build/$job_basename-$FLAVOR.yml; then
+if test -f $CONFIG_DIR/$job_basename-$FLAVOR.yml; then
   $SHELL rock-build-server "$@" $CONFIG_DIR/$job_basename-$FLAVOR.yml
 else
   $SHELL rock-build-server "$@" $CONFIG_DIR/default-$FLAVOR.yml
