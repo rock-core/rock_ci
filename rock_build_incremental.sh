c40d468dde2b1dfebe80f43fa4ffc82097a10833
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-08-29 18:31:27 +0200

    take the config in slave_conf/ for build-incremental as well

--- rock_build_incremental.sh
+++ rock_build_incremental.sh
@@ -10,9 +10,9 @@ cd $OLD_PATH
 export PATH=/home/build/rock_admin_scripts/bin:$PATH
 
 job_basename=`dirname $JOB_NAME`
-if test -f /home/build/$job_basename-$FLAVOR.yml; then
-  $SHELL rock-build-incremental "$@" /home/build/$job_basename-$FLAVOR.yml
+if test -f /home/build/slave_conf/$job_basename-$FLAVOR.yml; then
+  $SHELL rock-build-incremental "$@" /home/build/slave_conf/$job_basename-$FLAVOR.yml
 else
-  $SHELL rock-build-incremental "$@" /home/build/default-$FLAVOR.yml
+  $SHELL rock-build-incremental "$@" /home/build/slave_conf/default-$FLAVOR.yml
 fi
 
