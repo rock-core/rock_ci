d647289ce9148809d1168c63053ec1f92b062c73
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-09-16 10:34:51 +0200

    on the build server, just reset --hard to the remote master branch

--- update_slave_conf.sh
+++ update_slave_conf.sh
@@ -1,6 +1,8 @@
 #! /bin/sh -ex
 
 cd /home/build/rock_admin_scripts
-git pull
+git remote update
+git reset --hard origin/master
 cd /home/build/slave_conf
-git pull
+git remote update
+git reset --hard origin/master
