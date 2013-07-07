704a4ed237544310e1088866133323ff2e8c60e9
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-07-07 19:48:12 +0200

    we need to update very little now in update_slave_conf

--- update_slave_conf.sh
+++ update_slave_conf.sh
@@ -5,26 +5,7 @@
 sudo apt-get update
 sudo apt-get -y install wget ruby rubygems doxygen
 
-if test -z "$DONE_SLAVE_CONF"; then
-    cd /home/build/slave_conf
-    git remote update
-    git reset --hard origin/master
-    export DONE_SLAVE_CONF=1
-    exec $0
-fi
+cd $JENKINS_HOME/build_scripts
+git remote update
+git reset --hard origin/master
 
-if test -d /home/build/rock_buildconf; then
-    cd /home/build/rock_buildconf
-    git remote update
-    git reset --hard origin/master
-else
-    git clone git://gitorious.org/rock/buildconf-all.git /home/build/rock_buildconf
-fi
-
-if test -d /home/build/rock_admin_scripts; then
-    cd /home/build/rock_admin_scripts
-    git remote update
-    git reset --hard origin/master
-else
-    git clone git://gitorious.org/rock/admin_scripts.git /home/build/admin_scripts
-fi
